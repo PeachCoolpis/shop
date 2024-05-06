ae6319f3a4622e302dd5e7453a205e76161c7a88
PeachCoolPiece <121959157+PeachCoolPiece@users.noreply.github.com>
2024-05-07 00:03:16 +0900

    초기 세팅 2

---
+++ src/main/java/com/example/shop/entity/MemberRepository.java
@@ -0,0 +1,31 @@
+package com.example.shop.entity;
+
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+    
+    private final EntityManager em;
+    
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+    
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+    
+    public List<Member> findAll() {
+        return em.createQuery("select  m from Member m ", Member.class)
+                .getResultList();
+    }
+    
+    
+}
